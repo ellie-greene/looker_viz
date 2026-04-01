31e66709ea6a9f1e0e6e6cb90ee391531e1c1afd
ellie-greene <ellie.greene@purepetfood.co.uk>
2026-04-01 18:25:35 +0100

    update font

--- scorecards.js
+++ scorecards.js
@@ -1,33 +1,21 @@
 looker.plugins.visualizations.add({
   options: {},
   create: function(element, config) {
-    console.log("KPI VIZ: create called");
     element.innerHTML = '<div id="kpi-container" style="height:100%; display:flex; align-items:center; justify-content:center;"></div>';
   },
   updateAsync: function(data, element, config, queryResponse, details, done) {
-    console.log("KPI VIZ: updateAsync called");
-    console.log("KPI VIZ: data", JSON.stringify(data));
-    console.log("KPI VIZ: queryResponse", JSON.stringify(queryResponse));
-
     try {
-      var row        = data[0];
-      var fields     = queryResponse.fields;
-      var allFields  = fields.measures.concat(fields.dimensions);
-
-      console.log("KPI VIZ: allFields", allFields.map(f => f.name));
-      console.log("KPI VIZ: row", JSON.stringify(row));
+      var row       = data[0];
+      var fields    = queryResponse.fields;
+      var allFields = fields.measures.concat(fields.dimensions);
 
       var mainField   = allFields[0];
       var targetField = allFields[1];
       var ppField     = allFields[2];
 
-      var mainValue   = row[mainField.name].rendered  || row[mainField.name].value;
+      var mainValue   = row[mainField.name].rendered || row[mainField.name].value;
       var targetValue = row[targetField.name].value;
       var ppValue     = row[ppField.name].value;
-
-      console.log("KPI VIZ: mainValue", mainValue);
-      console.log("KPI VIZ: targetValue", targetValue);
-      console.log("KPI VIZ: ppValue", ppValue);
 
       var targetRendered = row[targetField.name].rendered || (targetValue * 100).toFixed(1) + '%';
       var ppRendered     = row[ppField.name].rendered    || (ppValue * 100).toFixed(1) + '%';
@@ -39,15 +27,14 @@ looker.plugins.visualizations.add({
         : '<span style="color:red;">▼</span>';
 
       var container = document.getElementById('kpi-container');
-      console.log("KPI VIZ: container", container);
 
       container.innerHTML = `
-        <div style="text-align:center; font-family:sans-serif;">
-          <div style="font-size:2.5em; font-weight:bold;">${mainValue}</div>
-          <div style="font-size:0.85em; color:grey; margin-top:4px;">
+        <div style="text-align:center; font-family: 'Inter', 'Roboto', sans-serif;">
+          <div style="font-size:2.5em; font-weight:600; color:#282828;">${mainValue}</div>
+          <div style="font-size:0.85em; color:#696969; margin-top:4px;">
             ${targetEmoji} ${targetRendered} vs target
           </div>
-          <div style="font-size:0.85em; margin-top:2px;">
+          <div style="font-size:0.85em; color:#696969; margin-top:2px;">
             ${ppArrow} ${ppRendered} vs prev. period
           </div>
         </div>
